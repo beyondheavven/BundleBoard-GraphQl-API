58b5ebc0cc84858bba3a97c2bbbed4d2cc707a16
beyondthegraveee <s24911@gmail.com>
2026-03-02 17:56:05 +0100

    created security config

--- src/main/java/com/source/bundleboard/user/mapper/UserMapper.java
+++ src/main/java/com/source/bundleboard/user/mapper/UserMapper.java
@@ -8,7 +8,6 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    @Mapping(target = "avatarUrl" , source = "avatarUrl")
     UserResponseDto toDto(User user);
 
 }
