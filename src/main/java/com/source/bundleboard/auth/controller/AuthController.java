972c9d17393d2107c0a7138820e86624271a1cc4
beyondthegraveee <s24911@gmail.com>
2026-03-03 17:45:30 +0100

    created login endpoint

--- src/main/java/com/source/bundleboard/auth/controller/AuthController.java
+++ src/main/java/com/source/bundleboard/auth/controller/AuthController.java
@@ -1,12 +1,25 @@
 package com.source.bundleboard.auth.controller;
 
+import com.source.bundleboard.auth.dto.AuthRequest;
+import com.source.bundleboard.auth.dto.AuthResponse;
 import com.source.bundleboard.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Mono;
 
 @Controller
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
+
+    @MutationMapping
+    public Mono<AuthResponse> login(@Argument AuthRequest request) {
+        return authService.authenticate(request)
+                .onErrorResume(e -> Mono.error(new RuntimeException(e.getMessage())));
+    }
+
+
 }
