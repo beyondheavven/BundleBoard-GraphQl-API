9b43e33bb8243e6fe6cc94749d142eb7a90d5a36
beyondthegraveee <s24911@gmail.com>
2026-03-06 11:49:33 +0100

    changed auth paths to rest

---
+++ src/main/java/com/source/bundleboard/constants/AuthApiPaths.java
@@ -0,0 +1,19 @@
+package com.source.bundleboard.constants;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public final class AuthApiPaths {
+
+    public static final String AUTH_PATH = "/auth";
+
+    public static final String REGISTER_PATH = "/register";
+
+    public static final String LOGIN_PATH = "/login";
+
+    public static final String LOGOUT_PATH = "/logout";
+
+    public static final String REFRESH_TOKEN = "/refresh";
+
+
+}
--- src/main/java/com/source/bundleboard/user/controller/UserController.java
+++ src/main/java/com/source/bundleboard/user/controller/UserController.java
@@ -3,7 +3,6 @@ package com.source.bundleboard.user.controller;
 import com.source.bundleboard.user.dto.UserResponseDto;
 import com.source.bundleboard.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
@@ -12,14 +11,12 @@ import reactor.core.publisher.Mono;
 
 @Controller
 @RequiredArgsConstructor
-@Slf4j
 public class UserController {
 
     private final UserService userService;
 
     @QueryMapping
     public Mono<UserResponseDto> getUserById(@Argument Long id){
-        log.info("getUserById: {}", id);
         return userService.getUserById(id);
     }
 
