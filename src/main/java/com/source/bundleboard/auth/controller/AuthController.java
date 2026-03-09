ee2d2d3f98831e92473c939d7de3d8fe46d4b45f
beyondthegraveee <s24911@gmail.com>
2026-03-09 14:43:39 +0100

    created cors and oath2 server

--- src/main/java/com/source/bundleboard/auth/controller/AuthController.java
+++ src/main/java/com/source/bundleboard/auth/controller/AuthController.java
@@ -8,8 +8,6 @@ import com.source.bundleboard.auth.service.AuthService;
 import com.source.bundleboard.constants.AuthApiPaths;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.graphql.data.method.annotation.Argument;
-import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
