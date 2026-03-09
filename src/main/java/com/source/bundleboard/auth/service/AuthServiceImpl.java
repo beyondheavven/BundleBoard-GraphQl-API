b82eaa5096f70771590e61ffb0d90db49245ee8a
beyondthegraveee <s24911@gmail.com>
2026-03-09 15:17:22 +0100

    quickfix for cors

--- src/main/java/com/source/bundleboard/auth/service/AuthServiceImpl.java
+++ src/main/java/com/source/bundleboard/auth/service/AuthServiceImpl.java
@@ -14,6 +14,7 @@ import com.source.bundleboard.user.model.UserRole;
 import com.source.bundleboard.user.model.UserStatus;
 import com.source.bundleboard.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -120,7 +121,8 @@ public class AuthServiceImpl implements AuthService {
     @Override
     public Mono<Void> logout(RefreshTokenRequest refreshTokenRequest) {
         String refreshToken = refreshTokenRequest.refreshToken();
-        return refreshTokenRepository.deleteByToken(refreshToken).then();
+        refreshTokenRepository.deleteByToken(refreshToken);
+        return Mono.empty();
     }
 
     private Mono<AuthResponse> generateAuthResponse(User user) {
