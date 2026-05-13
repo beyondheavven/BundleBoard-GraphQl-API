f527d82ddc542a6454b60701719492d5ad8d618a
beyondthegraveee <s24911@gmail.com>
2026-05-13 12:32:40 +0200

    created purchase model

---
+++ src/main/java/com/source/bundleboard/purchase/model/PurchaseStatus.java
@@ -0,0 +1,5 @@
+package com.source.bundleboard.purchase.model;
+
+public enum PurchaseStatus {
+    pending, succeeded, failed, refunded
+}
---
+++ src/main/java/com/source/bundleboard/purchase/service/PurchaseServiceImpl.java
@@ -0,0 +1,7 @@
+package com.source.bundleboard.purchase.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PurchaseServiceImpl implements PurchaseService {
+}
