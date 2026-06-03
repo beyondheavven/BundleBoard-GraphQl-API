5190c08ede7082a2cfe94122afe6a3b1eb6a4471
Kiryl Kurzau <jackertfun228@mail.ru>
2026-06-03 15:16:07 +0200

    added controller for payment

--- src/main/java/com/source/bundleboard/payment/controller/PaymentController.java
+++ src/main/java/com/source/bundleboard/payment/controller/PaymentController.java
@@ -2,11 +2,11 @@ package com.source.bundleboard.payment.controller;
 
 import com.source.bundleboard.payment.dto.PaymentRequest;
 import com.source.bundleboard.payment.service.PaymentService;
-import com.stripe.exception.StripeException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
 
@@ -17,7 +17,8 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     @MutationMapping
-    public Mono<String> createCheckoutSession(@Argument @Valid PaymentRequest input) throws StripeException {
+    @PreAuthorize("hasAnyRole('CLIENT', 'AUTHOR', 'ADMIN')")
+    public Mono<String> createCheckoutSession(@Argument @Valid PaymentRequest input) {
         return paymentService.createCheckoutSession(input);
     }
 
