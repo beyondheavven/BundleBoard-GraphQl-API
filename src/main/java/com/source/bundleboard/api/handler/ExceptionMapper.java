dfc5231f46f41196aa31b9c7a875f4b3c1f2a1f2
Kiryl Kurzau <jackertfun228@mail.ru>
2026-06-10 20:06:03 +0200

    added duplicate key exception to resolver

--- src/main/java/com/source/bundleboard/api/handler/ExceptionMapper.java
+++ src/main/java/com/source/bundleboard/api/handler/ExceptionMapper.java
@@ -1,6 +1,7 @@
 package com.source.bundleboard.api.handler;
 
 import com.source.bundleboard.api.exception.*;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
@@ -32,6 +33,18 @@ public class ExceptionMapper {
     }
 
     public ErrorCode getCode(Throwable ex) {
+        if (ex instanceof DuplicateKeyException) {
+            String msg = ex.getMessage();
+            if (msg != null) {
+                if (msg.contains("users_username_key")) {
+                    return ErrorCode.USERNAME_ALREADY_EXISTS;
+                }
+                if (msg.contains("users_email_key")) {
+                    return ErrorCode.EMAIL_ALREADY_EXISTS;
+                }
+            }
+            return ErrorCode.USER_ALREADY_EXISTS;
+        }
         return MAPPER.getOrDefault(ex.getClass(), ErrorCode.INTERNAL_SERVER_ERROR);
     }
 }
