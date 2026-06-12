df0c31c138a4b1718821484ae95a0a4df4ebc1eb
Kiryl Kurzau <jackertfun228@mail.ru>
2026-06-12 15:18:58 +0200

    added preauthorize for password controller

--- src/main/java/com/source/bundleboard/password/controller/PasswordController.java
+++ src/main/java/com/source/bundleboard/password/controller/PasswordController.java
@@ -9,6 +9,7 @@ import com.source.bundleboard.password.service.PasswordService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -21,21 +22,25 @@ public class PasswordController {
     private final PasswordService passwordService;
 
     @MutationMapping
+    @PreAuthorize("isAuthenticated()")
     public Mono<PasswordChangeResponse> requestPasswordChange(@Argument PasswordChangeInput input, @AuthenticationPrincipal UserDetails user){
         return passwordService.requestPasswordChange(input, user);
     }
 
     @MutationMapping
+    @PreAuthorize("isAuthenticated()")
     public Mono<PasswordChangeResponse> confirmPasswordChange(@Argument String code, @AuthenticationPrincipal UserDetails user){
         return passwordService.confirmPasswordChange(code, user);
     }
 
     @MutationMapping
+    @PreAuthorize("permitAll()")
     public Mono<PasswordResetResponse> requestPasswordReset(@Argument PasswordResetInput input){
         return passwordService.requestPasswordReset(input);
     }
 
     @MutationMapping
+    @PreAuthorize("permitAll()")
     public Mono<PasswordResetResponse> confirmPasswordReset(@Argument PasswordConfirmResetInput input){
         return passwordService.confirmPasswordReset(input);
     }
