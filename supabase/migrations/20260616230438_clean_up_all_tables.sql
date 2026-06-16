733765843fea7bc9f7ea64efd7a0130bcfddd189
Kiryl Kurzau <jackertfun228@mail.ru>
2026-06-17 01:14:52 +0200

    created clean migration for db

---
+++ supabase/migrations/20260616230438_clean_up_all_tables.sql
@@ -0,0 +1,22 @@
+SET session_replication_role = 'replica';
+
+TRUNCATE TABLE
+    authors,
+    clients,
+    collection_images,
+    collection_likes,
+    collection_tags,
+    collections,
+    purchase_items,
+    purchases,
+    refresh_token,
+    password_reset_tokens,
+    email_verification_token,
+    tags,
+    comments,
+    media_resources,
+    users
+    RESTART IDENTITY CASCADE;
+
+-- Включаем обратно
+SET session_replication_role = 'origin';
