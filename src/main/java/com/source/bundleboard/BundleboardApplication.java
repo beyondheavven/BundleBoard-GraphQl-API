49b7143dbaf5a0be58a38f6adcd6041f145f0162
Kiryl Kurzau <jackertfun228@mail.ru>
2026-06-18 11:11:22 +0200

    disabled redis repositories

--- src/main/java/com/source/bundleboard/BundleboardApplication.java
+++ src/main/java/com/source/bundleboard/BundleboardApplication.java
@@ -3,7 +3,9 @@ package com.source.bundleboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        Redis
+})
 public class BundleboardApplication {
 
 	public static void main(String[] args) {
