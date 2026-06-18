b44ca9f3afb6f3907377d3803b9f37a82c7a39bb
Kiryl Kurzau <jackertfun228@mail.ru>
2026-06-18 11:24:18 +0200

    removed annotation

--- src/main/java/com/source/bundleboard/BundleboardApplication.java
+++ src/main/java/com/source/bundleboard/BundleboardApplication.java
@@ -3,9 +3,7 @@ package com.source.bundleboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-        Redis
-})
+@SpringBootApplication
 public class BundleboardApplication {
 
 	public static void main(String[] args) {
