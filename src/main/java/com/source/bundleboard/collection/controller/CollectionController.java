65057cbf589d7f23ddcd24bc3e6958321324831d
Kiryl Kurzau <jackertfun228@mail.ru>
2026-07-01 02:18:31 +0200

    get liked collections must be also for clients

--- src/main/java/com/source/bundleboard/collection/controller/CollectionController.java
+++ src/main/java/com/source/bundleboard/collection/controller/CollectionController.java
@@ -89,7 +89,7 @@ public class CollectionController {
         return collectionService.getTopLikedCollections(actualLimit);
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN', 'AUTHOR')")
+    @PreAuthorize("hasAnyRole('ADMIN', 'CLIENT', 'AUTHOR')")
     @QueryMapping
     public Flux<CollectionResponse> getLikedCollections() {
         return collectionService.getLikedCollections();
